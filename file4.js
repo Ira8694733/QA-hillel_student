8e2ac9cf7784b788ca91519c3ddb0a1cc3c5853f
Ira Galtsova <iragaltsova7@gmail.com>
2024-03-30 21:37:14 +0200

    added lesson_7_hw.js

--- file4.js
+++ file4.js
@@ -71,3 +71,18 @@ function multipleOfTree(count) {
 }
 
 multipleOfTree(100);
+
+
+function sumOfNumbers(sum) {
+    sum = sum || 0;
+
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 !== 0) {
+            sum += i;
+        }
+    }
+
+    console.log(sum);
+}
+
+sumOfNumbers();
